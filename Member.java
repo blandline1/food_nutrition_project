f42c9fd99bc4f08ead8da5b9a8f68d1bf1fb4957
blandline <120055126+blandline@users.noreply.github.com>
2024-11-19 19:50:06 +0800

    hahahaha

--- Member.java
+++ Member.java
@@ -8,4 +8,23 @@ public class Member extends User {
     public String toString() {
         return "Member [name=" + name + ", id=" + id + "]";
     }
+
+    public void addFoodLog(Food food) {
+        dailyFoodLogs.add(food);
+    }
+
+    public void addWorkoutLog(Workout workout) {
+        dailyWorkoutLogs.add(workout);
+    }
+
+    public void showLogs() {
+        System.out.println("\nFood Logs:");
+        for (Food food : dailyFoodLogs) {
+            System.out.println(food);
+        }
+        System.out.println("\nWorkout Logs:");
+        for (Workout workout : dailyWorkoutLogs) {
+            System.out.println(workout);
+        }
+    }
 }
