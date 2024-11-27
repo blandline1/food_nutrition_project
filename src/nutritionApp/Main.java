8308153376dda3912cea64ba06afbdc877de182b
ahmadkhan44217 <64464651+ahmadkhan44217@users.noreply.github.com>
2024-11-27 22:54:02 +0800

    Authentication refactor

--- src/nutritionApp/Main.java
+++ src/nutritionApp/Main.java
@@ -1,13 +1,14 @@
 package nutritionApp;
+
 import java.util.*;
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner scanner = new Scanner(new File("1.txt"));
-        //runningProgram(scanner);
-        scanner.close();
-        scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(new File("1.txt"));
+//        //runningProgram(scanner);
+//        scanner.close();
+        Scanner scanner = new Scanner(System.in);
         runningProgram(scanner);
 
     }
@@ -17,57 +18,9 @@ public class Main {
 
         try (scanner) {
             while (!exit) {
-                User loggedUser = null;
-                while (loggedUser == null && !exit) {
-
-                    System.out.println("\nWelcome to the Food Nutrition App Authenticator!");
-                    System.out.println("1. Member sign up");
-                    System.out.println("2. Trainer sign up");
-                    System.out.println("3. Log in");
-                    System.out.println("-1. Exit");
-
-                    int choice = scanner.nextInt();
-                    scanner.nextLine();
-                    Authenticator authenticator = Authenticator.getInstance();
-                    String name = "";
-                    String password = "";
-
-
-                    if (choice != -1) {
-
-                        System.out.print("Please enter your username: ");
-                        name = scanner.nextLine();
-                        System.out.print("Please enter your password: ");
-                        password = scanner.nextLine();
-                    }
-
-
-                    switch (choice) {
-                        case 1:
-                            System.out.print(authenticator.MemberSignUp(name, password));
-                            break;
-                        case 2:
-                            System.out.print(authenticator.TrainerSignUp(name, password));
-                            break;
-                        case 3:
-                            loggedUser = authenticator.Login(name, password);
-                            if (loggedUser == null) {
-                                System.out.print("Invalid credentials");
-                            }
-                            break;
-                        case -1:
-                            exit = true;
-                            System.out.println("Fuck off!");
-                            break;
-                        default:
-                            System.out.println("Invalid choice. Please try again.");
-                    }
-
-                }
-
-                if (!exit) {
-                    System.out.println("\nWelcome to the Food Nutrition App!");
-                }
+                Authenticator authenticator = Authenticator.getInstance();
+                exit = authenticator.LoginPage(scanner);
+                User loggedUser = authenticator.getLoggedUser();
 
                 while (!exit && loggedUser != null) {
 
@@ -79,7 +32,7 @@ public class Main {
                     switch (choice) {
                         case -1:
                             loggedUser = null;
-                            System.out.println("Thank you for using Food Nutrition App!");
+                            System.out.println(authenticator.Logout());
                             break;
                         case 1:
                             loggedUser.runOpt1(scanner);
@@ -103,5 +56,10 @@ public class Main {
         }finally {
             scanner.close();
         }
+    }
+
+    private static void initData() throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("InitData.txt"));
+
     }
 }
