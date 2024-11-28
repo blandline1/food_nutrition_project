1279bdb466320825ed55e8a0ca1494bcab184642
Syed Aahmad Adnan <93536721+Octane18@users.noreply.github.com>
2024-11-29 02:24:35 +0800

    Analysis validation added

--- src/nutritionApp/Analysis.java
+++ src/nutritionApp/Analysis.java
@@ -14,15 +14,24 @@ public class Analysis {
         return instance;
     }
 
-    public double func_Q(int xi, int xa) {
+    public double func_Q(int xi, int xa) throws ExMathError {
+    	if(xi < 0 || xa < 0) {
+    		throw new ExMathError();
+    	}
         return ((double) (xi - Math.abs(xa - xi)) /xi) * 100;
     }
 
-    public double food_biased_weight(int cal, int fats, int prot, int carbs, double hyd, double adher) {
+    public double food_biased_weight(int cal, int fats, int prot, int carbs, double hyd, double adher) throws ExMathError {
+    	if (cal < 0 || fats < 0 || prot < 0 || carbs < 0 || hyd < 0 || adher < 0) {
+    		throw new ExMathError();
+    	}
         return 0.3*cal + 0.1*fats + 0.15*prot + 0.15*carbs + 0.2*hyd + 0.1*adher;
     }
 
-    public double workout_biased_weight(int sets, int reps, int minutes) {
+    public double workout_biased_weight(int sets, int reps, int minutes) throws ExMathError{
+    	if (sets<0 || reps < 0 || minutes < 0) {
+    		throw new ExMathError();
+    	}
         return 0.3*sets + 0.3*reps + 0.4*minutes;
     }
 
@@ -36,7 +45,11 @@ public class Analysis {
         return ((double) times_expected /expected.size()) * 100;
     }
 
-    public double hyd_score(double x, double cap) {
+    public double hyd_score(double x, double cap) throws ExMathError{
+    	
+    	if(x<0 || cap<0) {
+    		throw new ExMathError();
+    	}
         if(x>=cap) {
             return 100;
 
@@ -48,54 +61,64 @@ public class Analysis {
 
     public void conductFoodAnalysis(ArrayList<Food> expected, ArrayList<Food> actual) {
 
-        double ind_cal = 0.0;
-        double ind_fats = 0.0;
-        double ind_prot = 0.0;
-        double ind_carbs = 0.0;
-        double ind_hyd = 0.0;
+       try {
+    	   if(expected == null || actual == null) {
+    		   throw new ExEmptyInput();
+    	   }
+    	   double ind_cal = 0.0;
+           double ind_fats = 0.0;
+           double ind_prot = 0.0;
+           double ind_carbs = 0.0;
+           double ind_hyd = 0.0;
 
-        int total_cal = 0;
-        int total_fats = 0;
-        int total_prot = 0;
-        int total_carbs = 0;
-        double total_hyd = 0;
-        double adher = 0.0;
+           int total_cal = 0;
+           int total_fats = 0;
+           int total_prot = 0;
+           int total_carbs = 0;
+           double total_hyd = 0;
+           double adher = 0.0;
 
-        double total_biased_weight = 0.0;
-        for (int i=0; i<7; i++) {
+           double total_biased_weight = 0.0;
+           for (int i=0; i<7; i++) {
 
-            total_cal += actual.get(i).getCalories();
-            total_fats += actual.get(i).getFats();
-            total_prot += actual.get(i).getProteins();
-            total_carbs += actual.get(i).getCarbs();
-            total_hyd += actual.get(i).getWaterIntake();
-            ind_cal += func_Q(expected.get(i).getCalories(), actual.get(i).getCalories());
-            ind_fats += func_Q(expected.get(i).getFats(), actual.get(i).getFats());
-            ind_prot += func_Q(expected.get(i).getProteins(), actual.get(i).getProteins());
-            ind_carbs += func_Q(expected.get(i).getCarbs(), actual.get(i).getCarbs());
-            adher += adherence_calculator(expected.get(i).getFoodList(), actual.get(i).getFoodList());
-            ind_hyd += hyd_score(actual.get(i).getWaterIntake(), expected.get(i).getWaterIntake());
+               total_cal += actual.get(i).getCalories();
+               total_fats += actual.get(i).getFats();
+               total_prot += actual.get(i).getProteins();
+               total_carbs += actual.get(i).getCarbs();
+               total_hyd += actual.get(i).getWaterIntake();
+               ind_cal += func_Q(expected.get(i).getCalories(), actual.get(i).getCalories());
+               ind_fats += func_Q(expected.get(i).getFats(), actual.get(i).getFats());
+               ind_prot += func_Q(expected.get(i).getProteins(), actual.get(i).getProteins());
+               ind_carbs += func_Q(expected.get(i).getCarbs(), actual.get(i).getCarbs());
+               adher += adherence_calculator(expected.get(i).getFoodList(), actual.get(i).getFoodList());
+               ind_hyd += hyd_score(actual.get(i).getWaterIntake(), expected.get(i).getWaterIntake());
 
 
-        }
-        total_cal /= 7;
-        total_fats /= 7;
-        total_prot /= 7;
-        total_carbs /= 7;
-        adher /= 7;
-        ind_cal /= 7;
-        ind_fats /= 7;
-        ind_prot /= 7;
-        ind_carbs /= 7;
-        ind_hyd /= 7;
+           }
+           total_cal /= 7;
+           total_fats /= 7;
+           total_prot /= 7;
+           total_carbs /= 7;
+           adher /= 7;
+           ind_cal /= 7;
+           ind_fats /= 7;
+           ind_prot /= 7;
+           ind_carbs /= 7;
+           ind_hyd /= 7;
 
-        total_hyd /= 7;
-        total_biased_weight = food_biased_weight(total_cal, total_fats, total_prot, total_carbs, total_hyd, adher);
+           total_hyd /= 7;
+           
+           
+           total_biased_weight = food_biased_weight(total_cal, total_fats, total_prot, total_carbs, total_hyd, adher);
+           
+           System.out.println("Individual scores");
+           System.out.printf("Calories: %.2f, Fats: %.2f, Proteins: %.2f, Carbs: %.2f, Water Intake: %.2f\n", ind_cal, ind_fats, ind_prot, ind_carbs, ind_hyd);
+           System.out.printf("Overall score: %.2f", total_biased_weight);
 
-        System.out.println("Individual scores");
-        System.out.printf("Calories: %.2f, Fats: %.2f, Proteins: %.2f, Carbs: %.2f, Water Intake: %.2f\n", ind_cal, ind_fats, ind_prot, ind_carbs, ind_hyd);
-        System.out.printf("Overall score: %.2f", total_biased_weight);
-
+       }
+       catch(ExMathError | ExEmptyInput e) {
+    	   System.out.println(e.getMessage());
+       }
 
 
 
@@ -103,61 +126,71 @@ public class Analysis {
 
     public void conductWorkoutAnalysis(ArrayList<ArrayList<Workout>> expected, ArrayList<ArrayList<Workout>> actual) {
 
-        int total_sets = 0;
-        int total_reps = 0;
-        int total_minutes = 0;
+    	try {
+    		if(expected == null || actual == null) {
+    			throw new ExEmptyInput();
+    		}
+    	       int total_sets = 0;
+    	        int total_reps = 0;
+    	        int total_minutes = 0;
 
-        int final_sets = 0;
-        int final_reps = 0;
-        int final_minutes = 0;
+    	        int final_sets = 0;
+    	        int final_reps = 0;
+    	        int final_minutes = 0;
 
 
-        double ind_sets = 0.0;
-        double ind_reps = 0.0;
-        double ind_minutes = 0.0;
-        double adherence = 0.0;
+    	        double ind_sets = 0.0;
+    	        double ind_reps = 0.0;
+    	        double ind_minutes = 0.0;
+    	        double adherence = 0.0;
 
-        for (int i = 0; i < 7; i++) {
+    	        for (int i = 0; i < 7; i++) {
 
-            total_sets = 0;
-            total_reps = 0;
-            total_minutes = 0;
+    	            total_sets = 0;
+    	            total_reps = 0;
+    	            total_minutes = 0;
 
-            ArrayList<String> expected_wk = new ArrayList<>();
-            ArrayList<String> actual_wk = new ArrayList<>();
-            int size = Math.max(expected.get(i).size(), actual.get(i).size());
-            for (int j = 0; j < size; j++) {
-                expected_wk.add(expected.get(i).get(j).getName());
-                actual_wk.add(actual.get(i).get(j).getName());
+    	            ArrayList<String> expected_wk = new ArrayList<>();
+    	            ArrayList<String> actual_wk = new ArrayList<>();
+    	            int size = Math.max(expected.get(i).size(), actual.get(i).size());
+    	            for (int j = 0; j < size; j++) {
+    	                expected_wk.add(expected.get(i).get(j).getName());
+    	                actual_wk.add(actual.get(i).get(j).getName());
 
-                ind_sets += func_Q(expected.get(i).get(j).getSets(), actual.get(i).get(j).getSets());
-                ind_reps += func_Q(expected.get(i).get(j).getReps(), actual.get(i).get(j).getReps());
-                ind_minutes += func_Q(expected.get(i).get(j).getMinutes(), actual.get(i).get(j).getMinutes());
+    	                ind_sets += func_Q(expected.get(i).get(j).getSets(), actual.get(i).get(j).getSets());
+    	                ind_reps += func_Q(expected.get(i).get(j).getReps(), actual.get(i).get(j).getReps());
+    	                ind_minutes += func_Q(expected.get(i).get(j).getMinutes(), actual.get(i).get(j).getMinutes());
 
-                total_sets += expected.get(i).get(j).getSets();
-                total_reps += actual.get(i).get(j).getReps();
-                total_minutes += expected.get(i).get(j).getMinutes();
-            }
-            ind_sets /= size;
-            ind_reps /= size;
-            ind_minutes /= size;
+    	                total_sets += expected.get(i).get(j).getSets();
+    	                total_reps += actual.get(i).get(j).getReps();
+    	                total_minutes += expected.get(i).get(j).getMinutes();
+    	            }
+    	            ind_sets /= size;
+    	            ind_reps /= size;
+    	            ind_minutes /= size;
 
-            total_sets /= size;
-            total_reps /= size;
-            total_minutes /= size;
+    	            total_sets /= size;
+    	            total_reps /= size;
+    	            total_minutes /= size;
 
-            final_sets += total_sets;
-            final_reps += total_reps;
-            final_minutes += total_minutes;
+    	            final_sets += total_sets;
+    	            final_reps += total_reps;
+    	            final_minutes += total_minutes;
 
-            adherence = adherence_calculator(expected_wk, actual_wk);
+    	            adherence = adherence_calculator(expected_wk, actual_wk);
+    	            
+    	           
 
-            System.out.println("Day 1 individual scores: ");
-            System.out.printf("Sets: %.2f, Reps: %.2f, Minutes: %.2f, Adherence: %.2f\n", ind_sets, ind_reps, ind_minutes, adherence);
+    	            System.out.println("Day 1 individual scores: ");
+    	            System.out.printf("Sets: %.2f, Reps: %.2f, Minutes: %.2f, Adherence: %.2f\n", ind_sets, ind_reps, ind_minutes, adherence);
 
-        }
+    	        }
 
-        double total_biased_weight = workout_biased_weight(final_sets, final_reps, final_minutes);
-        System.out.printf("Overall biased score: %.2f", total_biased_weight);
+    	        double total_biased_weight = workout_biased_weight(final_sets, final_reps, final_minutes);
+    	        System.out.printf("Overall biased score: %.2f", total_biased_weight);
+    	}
+    	catch (ExEmptyInput | ExMathError e) {
+    		System.out.println(e.getMessage());
+    	}
     }
 }
